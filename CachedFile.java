5483524b54599e61f20d266b5404a9ba62ecad6f
gesingle <gesingle@users.noreply.github.com>
2017-06-06 21:21:48 -0700

    Updated CachedFile and FileServer

--- CachedFile.java
+++ CachedFile.java
@@ -1,16 +1,134 @@
+import java.io.*;
+import java.rmi.RemoteException;
+import java.util.Vector;
 
+/**
+ * Class represents a file cached in the server
+ * 
+ * @version CSS434 DFS 
+ */
 public class CachedFile {
 
+	public static final int NOT_SHARED = 0;
+	public static final int READ_SHARED = 1;
+	public static final int WRITE_SHARED = 2;
+	public static final int OWNERSHIP_CHANGE = 3;
 	
-	private ArrayList<ClientProxy> readers;
-	private String owner;
-	private String state;
-	private File storedFile;		//reference to the file stored on disk
-	private byte[] data;			//file's current contents - an in-memory cache
 
-	public CachedFile() {
+	private Vector<ClientProxy> readers;
+	private ClientProxy owner;
+	public int state;
+	private File file;
+	private byte[] data;	
+	private FileInputStream fis;
+	private FileOutputStream fos;
 
+	/** 
+	 * Constructor
+	 * 	
+	 * Caches a new file by reading and storing 
+	 * its contents from disk 
+	 */
+	public CachedFile(String filename) {
 
-
+		// file path
+		file = new File("/tmp/" + filename);
+		// get the file's data from disk
+		data = new byte[(int) file.length()];
+		fis = new FileInputStream(file);
+		fis.read(data);
+		
+		readers = new Vector<ClientProxy>();
+		state = NOT_SHARED;
 	}
+	
+	/**
+	 * Getter for file name
+	 */
+	public String getName(){
+		
+		return file.getName();
+	}
+	
+	
+	/**
+	 * Adds a new reader to the list of readers
+	 */
+	public void addReader(ClientProxy client){
+		
+		if(!readers.contains(client)){
+			readers.add(client);
+		}
+	}
+	
+	/**
+	 * Remove a client from the readers list
+	 */
+	public void removeReader(String client){
+		
+		for(ClientProxy cp : readers){
+			if(cp.getName().equals(client)){
+				readers.remove(cp);
+			}
+		}
+	}
+	
+	public void addWriter(ClientProxy client){
+		
+		if (!client.equals(owner)) {
+			synchronized (this) {
+                // Wait for current owner to writeBack() and release the file
+				while (owner != null) {
+					try {
+						owner.writeback();
+						wait();
+					} catch (RemoteException e) {
+						throw new RemoteException("Previous owner failed to writeback.");
+					}
+				}
+			}
+		} 
+		
+		owner = client;
+	}
+	
+	/**
+	 * Updates the contents of this file. 
+	 * 
+	 * The current owner relinquishes ownership and all readers
+	 * are invalidated
+	 */
+	public boolean update(FileContents contents){
+		
+		if (owner == null) {
+			return false;
+		}
+		
+        // Invalidate all readers.
+		for(ClientProxy cp : readers){
+			cp.invalidate();
+			readers.remove(cp);
+		}
+		
+		// previous owner is still a reader
+        readers.add(owner);
+        // but no longer owns the file
+		owner = null;
+		// update file contents in cache
+		data = contents.get();
+		// and on disk
+		fos = new FileOutputStream(file);
+		fos.write(contents.get(), 0, contents.get().length);
+		
+		// update state
+		state = (state == WRITE_SHARED) ? NOT_SHARED : WRITE_SHARED;
+		
+		synchronized (this) {
+			notifyAll();
+		}
+		
+		return true;
+	}
+	
+
 }
--- FileServer.java
+++ FileServer.java
@@ -5,55 +5,105 @@ import java.rmi.server.*;
 
 public class FileServer extends UnicastRemoteObject implements ServerInterface {
 
-	int port;						//port number
-	ArrayList<CachedFile> fileCache;		//all files currently cached
+	int port;								
+	Vector<CachedFile> cache;		
 
-	public FileServer(String port) throws RemoteException {
+	public FileServer(int port) throws RemoteException {
 
 		this.port = port;
-		fileCache = new ArrayList<CachedFile>();
+		cache = new Vector<CachedFile>();
 
 	}
 
+	public FileContents download(String clientIP, String filename, String mode) throws RemoteException{
+		
+		// pointer for the file
+		CachedFile file = getCachedFile(filename);
+		
+		// if file is not in cache, read it from disk 
+		if(file == null){
+			try{
+				file = new CachedFile(filename);
+			}
+			catch(IOException e){
+				throw new RemoteException("Requested file is not on disk.");
+			}
+			cache.add(file);
+		}
+		
+		// Register the client with the file
+		ClientProxy client = new ClientProxy(clientIP, port);
+		
+		// if read requested, add the client as a reader and update
+		// state if necessary
+		if(mode.equals("r")){
+			file.addReader(client);
+			if((file.state != CachedFile.WRITE_SHARED) && (file.state != CachedFile.OWNERSHIP_CHANGE)){
+				file.state = CachedFile.READ_SHARED;
+			}
+		}
+		// if write requested, add the client as a writer and update 
+		// state if necessary
+		else if(mode.equals("w")){
+			file.addWriter(client);
+			if(file.state == CachedFile.READ_SHARED){
+				file.state = CachedFile.WRITE_SHARED;
+			}
+			else if(file.state == CachedFile.WRITE_SHARED){
+				file.state = CachedFile.OWNERSHIP_CHANGE;
+			}
+		}	
+	}
+
+	/** 
+	 * Updates the contents of a file on client request
+	 */
+	public boolean upload( String client, String filename, FileContents contents ) throws RemoteException{
+		
+		CachedFile file = getCachedFile(filename);
+		
+		// return false if file is not in cache
+		if((file == null) || (file.state == CachedFile.NOT_SHARED) || (file.state == CachedFile.READ_SHARED)){
+			return false;
+		}
+		
+		return file.update(contents);
+		
+	}
+
+	/**
+	 * Searches the cache for a file. If the file is cached, it is 
+	 * returned, if not, a new cache entry is created for the file.
+	 */
+	private CachedFile getCachedFile(String filename){
+		
+		for(CachedFile file : cache){
+			if(file.getName().equals(filename)){
+				return file;
+			}
+		}
+		
+		return null;
+				
+	}
 
 	public static void main (String args[]) {
 
-		
 		if ( args.length != 1 ) {
-	    	System.err.println( "usage: java FileServer port#" );
-	    	System.exit( -1 );
+			System.err.println( "usage: java FileServer port#" );
+			System.exit( -1 );
 		}
 
 		try {
 			System.out.println("Starting server...");
-		    FileServer server = new FileServer(Integer.parseInt(args[0]));
+			FileServer server = new FileServer(Integer.parseInt(args[0]));
 
-		    //bind server name to ip adress
-		    Naming.rebind( "rmi://localhost:" + args[0] + "/fileserver", server );
+			//bind server name to ip adress
+			Naming.rebind( "rmi://localhost:" + args[0] + "/fileserver", server );
 			System.out.println("Server started.");
-
-		} catch ( Exception e ) {
-		    e.printStackTrace( );
-		    System.exit( 1 );
+		} 
+		catch ( Exception e ) {
+			System.exit( 1 );
 		}
-		
 	}
-	public FileContents download( String client, String filename, String mode )
-	throws RemoteException;
-    public boolean upload( String client, String filename, 
-			   FileContents contents ) throws RemoteException;
-
-
-
-    private CachedFile getCachedFile(String filename){
-    	for(CachedFile file : fileCache){
-    		if(file.getFileName().equals(filename)){
-    			return file;
-    		}
-    	}
-    	return null;
-    }
-
-
-
 }
